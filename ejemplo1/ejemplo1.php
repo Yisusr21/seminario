1085aadc03b044bdc194b18c00d1c8b20b3dc822
Yisusr21 <jrjimenez293@hotmail.com>
2025-05-01 21:12:53 -0300

    .

--- ejemplo1/ejemplo1.php
+++ ejemplo1/ejemplo1.php
@@ -19,16 +19,60 @@ if ($objBD -> connect_errno){
     echo "Hubo un intento de error en la base de datos: (". $objBD-> connect_errno .")" . $objBD -> connect_errno;
     exit;
 }
-
+$objBD->set_charset("utf8");
 //Select
 
 $objBD -> real_query("SELECT * FROM cliente ORDER BY id_cliente DESC" );
 $resultado = $objBD ->use_result();
 
-echo "Resultado:<br> ";
+echo "Resultado_BD:<br> ";
 while ($fila = $resultado ->fetch_assoc()){
     echo"cod: " . $fila["id_cliente"] . " - Nombre: " . $fila["nombre"] . "<br>";
 }
 
+
+
+if(isset($_POST['register'])){
+    if(strlen($_POST['name']) >= 1){
+        $nombre = trim($_POST['name']);
+        $consulta = "INSERT INTO cliente(nombre) values ('$nombre')";
+        $agg = mysqli_query($objBD,$consulta);
+            if($agg){
+                echo "<h3>Has agregado correctamente el nombre</h3>";
+            }
+            else{
+                echo "<h3>Algo hiciste mal</h3>";
+            }
+        }
+    if(strlen($_POST['name1']) >= 1 && $_POST['numero'] >= 1){
+        $nombre = trim($_POST['name1']);
+        $id = $_POST['numero'];
+        $consulta = "UPDATE cliente SET nombre = '$nombre' where id_cliente ='$id'";
+        $cambio = mysqli_query($objBD,$consulta);
+            if($cambio){
+                echo "<h3>Modificaste el cliente con el codigo '$id'</h3>";
+            }
+            else{
+                echo "<h3>No se logro ningun cambio</h3>";
+            }
+        }
+    elseif($_POST['numero'] >= 0){
+            $id = $_POST['numero'];
+            $consulta = "DELETE FROM Cliente WHERE id_cliente = '$id'";
+            $cambio = mysqli_query($objBD,$consulta);
+                if ($cambio){
+                    echo "<h3> Has eliminado al cliente con id '$id'</h3>";
+                    }
+                else{
+                    echo "<h3>Colocale un id causa</h3>";
+                    }
+        
+            }
+        
+}
+
+
+
 $objBD ->close();
+
 ?>
