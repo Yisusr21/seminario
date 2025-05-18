1f6ed64806b0cce4a9dba0ab3b4b680651d6c5fd
Yisusr21 <jrjimenez293@hotmail.com>
2025-05-18 19:59:07 -0300

    seminario

--- ejemplo2/index.php
+++ ejemplo2/index.php
@@ -2,11 +2,19 @@
 include "config.php";
 include "includes/class.inc.php";
 
-$BD = new DateBase();
+$BD = new DateBase("localhost","root","","ejemplo1");
 
 if(!$BD->getEstadoConexion()){
     echo $BD->getMensajeError();
     exit;
 }
+
+$aDatos = $BD->getQuery("SELECT * FROM cliente ORDER BY id_cliente ASC");
+for ($i = 0; $i < sizeof($aDatos); $i++){
+    echo "cod:" . $aDatos[$i]["id_cliente"] . "Nombre: " .$aDatos[$i]["nombre"]. "<BR>";
+}
+
+
+$BD->close()
 
 ?>
